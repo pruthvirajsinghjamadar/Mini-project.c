00a6ce939ceb7f01bd759ac5fee758ae8fb43bde
pruthvirajsinghjamadar <pruthvirajsinghjamadar@gmail.com>
2025-11-26 16:26:55 +0530

    Add initial anime suggestion program
    This my mini project where my code will suggest you a anime based on the theme You choose
         If you watched before it then it will suggess you more anime at completion it will also help if you don't know the which theme you want you can choose the best anime just see it go through it have fun

---
+++ Animesuggester.c
@@ -0,0 +1,379 @@
+#include<stdio.h>
+
+void main(){
+    int name;
+    char theme;
+
+    printf("what type of theme are you looking for  \n");
+     
+    printf("Themes are : 1.psycology \n 2.comedy\n 3.power\n 4.action\n 5.mystery\n6.romance\n 7.goated\n 8.hentai\n 9.underrated\n &.story line :");
+    scanf("%c",&theme);
+
+    switch (theme)
+    {
+    case'1':
+        printf("CLASSROOM OF THE ELITE");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("DEATH NOTE");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("MONSTER");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                        
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+        
+        case '2':
+                printf("SAIKI.K");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("GRAND BLUE");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("BLUE LOCK");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+
+        case '3':
+                printf("DRAGON BALL");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("ONE PUNCH MAN");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("BLUE LOCK");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+
+        case '4':
+                printf("NARUTO");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("SOLO LEVELING");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("MY HERO ACADEMIA");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+
+        case'5':
+        printf("TOWN WHERE PEOPLE SUDDENLY DISAPEAR");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("CASE STUDY OF VANITAS");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("I RRECARNETED AS A SLIME");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+        
+        case '6':
+                printf("LOVE IS WAR");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("BANNY GIRL SENPAI");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("HORIMIYA");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+
+        case '7':
+                printf("ONE PIECE");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("NARUTO");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("BLEACH");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+            
+        case'8':
+        printf("OVERFLOW");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("HIGH SCHOOL DXD");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("MY 100 GF WHO REALLY REALLY REALLY REALLY REALLY LOVE ME");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+        
+        case '9':
+                printf("FULL METAL ALCHEMIST");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("HUNTER X HUNTER");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("PROMISSED NEVERLAND");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+
+        case'&':
+        printf("ATTACK ON TITAN");
+        printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+        scanf("%d",&name);
+        if(name==1){
+            printf("FULL METAL ALCHEMIST");
+            printf("HAve you ever seen this before, if yes enter 1 if no enter 0 ");
+            scanf("%d",&name);
+                if(name==1){
+                     printf("NARUTO");
+                }
+                else{
+                    printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                    scanf("%d",&name);
+                        if(name==1){
+                            printf("enterr the enter boton");
+                        }
+                        else{
+                            printf("Enjoy your anime😊");
+                        }
+                }
+            
+        }
+        else{
+                printf("you can watch this anime or if you want to change the theme enter 1 or enter 0");
+                scanf("%d",&name);
+                     if(name==1){
+                         printf("enterr the enter boton");
+                     }
+                     else{
+                       printf("Enjoy your anime😊");
+                        }
+                }
+        
+        break;
+        
+        default:
+        printf("you can start your anime journey with these \n 1.DEMON SLAYER \n2.TOKYO REVENGERS \n 3.CLASS ROOM OF THE ELITE");
+    
+            
+        }
+    }
